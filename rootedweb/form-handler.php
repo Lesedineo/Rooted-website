fef409f2ca5c3d6ac64d1e7efcdd964207d85acc
Lesedineo <133144071+Lesedineo@users.noreply.github.com>
2025-08-05 19:16:39 +0200

    Updated the home page Html and css

--- rootedweb/form-handler.php
+++ rootedweb/form-handler.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from= '@lmboitumelo@gmail.com';
+$email_from= 'info@rootedwebsite.com';
 
 $email_subject = "New Form Submission";
 
